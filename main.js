f617d510abea8d5797fde56ecefbbe2e36aef14c
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2021-05-21 20:11:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+images=["img1.JPG","ing2.JPG"];
+names=["Myfamily","familyonatrip"];
+var i=0;
+function next(){
+    document.getElementById("family_img").innerHTML=images[i];
+    document.getElementById("family_name").innerHTML=names[i];
+    i++;
+    if (i==2){
+        i=0;
+    }
+}
+
+
