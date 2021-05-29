8c36553fdab8aed6a09ac78def6e8566c1dff9ac
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2021-05-28 20:51:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ images=["img1.JPG","ing2.JPG"];
 names=["Myfamily","familyonatrip"];
 var i=0;
 function next(){
-    document.getElementById("family_img").innerHTML=images[i];
+    document.getElementById("family_img").src=images[i];
     document.getElementById("family_name").innerHTML=names[i];
     i++;
     if (i==2){
